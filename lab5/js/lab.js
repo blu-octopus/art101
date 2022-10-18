f965c16209da7f7461c3127873c744c51dce675b
Daphne Cheng <dcheng11@ucsc.edu>
2022-10-17 23:55:52 -0700

    lab 5 done

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Daphne Cheng, Million Villanueva
+ * Created:   10.07.2022
+ *
+ **/
+
+ // Define Variables
+make = "Squishmellow";
+model= "Wendy the Frog";
+color= "Green";
+year= 2017;
+ownIt = false;
+
+//calculate
+age = 2022 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years <br>");
