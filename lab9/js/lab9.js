bbb1ea1c50a1e69a36a159d8cd18f99eda5c8daf
Daphne Cheng <dcheng11@ucsc.edu>
2022-10-30 22:59:00 -0700

    lab 9

---
+++ lab9/js/lab9.js
@@ -0,0 +1,37 @@
+//1
+/**
+ * Author:    Daphne Cheng
+ * Created:   10.26.2022
+ *
+ **/
+
+ //2 Using getElementById()
+var outputEl = document.getElementById("output");
+console.log("outputEl: ", outputEl);
+
+//3 document.createElement
+//create <p></p>
+var new1El = document.createElement("p");
+
+//4 html attribute
+//innerHTML for <p> with id new1EL
+new1El.innerHTML = "Something new.";
+//id to go in css
+new1El.id = "new-one";
+
+//5 new element
+//create <p></p>
+
+var new2El = document.createElement("p");
+//id to go in css
+new2El.id = "new-two";
+
+//6 element2 says something else
+//innerHTML for <p> with id new2EL
+new2El.innerHTML = "Something else.";
+
+//7 appened child
+outputEl.appendChild(new1El);
+outputEl.appendChild(new2El);
+
+//8 change css attribute?
