3a08415a0b066abe544f48b3f480989cf2db6e24
Daphne Cheng <dcheng11@ucsc.edu>
2022-11-08 18:09:27 -0800

    bonus task

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -5,15 +5,32 @@
  *
  **/
 
+ //copied from professor Modes' website
+ function checksum(s) {
+   var hash = 0, strlen = s.length, i, c;
+   if ( strlen === 0 ) {
+     return hash;
+   }
+   for ( i = 0; i < strlen; i++ ) {
+     c = s.charCodeAt( i );
+     hash = ((hash << 5) - hash) + c;
+     hash = hash & hash; // Convert to 32bit integer
+   }
+   return hash;
+ };
+
  //task 2
  var houses = ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"];
  var descriptions = ["you are brave", "you are smart", "you are detail oriented", "you are kind"]
 
  function sortingHat(str) {
    len = str.length;
-   mod = len % houses.length;
+   console.log("length is ", len);
+   hashedValue = checksum(str);
+   console.log("hashedValue is ", hashedValue);
+   mod = Math.abs(hashedValue) % houses.length;
    desc = descriptions[mod];
-   console.log("str2 is ", desc);
+   console.log("desc is updated to ", desc);
    return houses[mod];
    // if (mod == 0) {
    //   return "Gryffindor"
@@ -30,13 +47,12 @@
  }
 
  var myButton = document.getElementById("button");
- var name = document.getElementById("input").value;
  var desc = "description of houses";
 
  myButton.addEventListener("click", function() {
-  var house = sortingHat(name, desc);
-  console.log(desc);
-
+   var name = document.getElementById("input").value;
+   console.log("input is ", input);
+  var house = sortingHat(name);
   newText = "<p> The Sorting Hat has sorted you into "+
           "<h2>" + house + "!</h2>"+
           "<p>" + desc + "</p>";
