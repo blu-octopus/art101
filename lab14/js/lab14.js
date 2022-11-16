2fa99214d3c61e47e7718c3106cf96c9dca636f6
Daphne Cheng <dcheng11@ucsc.edu>
2022-11-15 18:18:49 -0800

    lab 14

---
+++ lab14/js/lab14.js
@@ -0,0 +1,21 @@
+//1
+/**
+ * Author:    Daphne Cheng
+ * Created:   11.14.2022
+ *
+ **/
+ console.log("in js")
+
+ function myname(){
+   console.log("ran myName")
+   document.write("Monique");
+ }
+ function welcomeW(name){
+  console.log("ran welcomeW")
+ 	document.write("Welcome " + name + "!");
+ }
+
+ function welcomeR(name){
+  console.log("ran welcomeR")
+ 	return "Welcome "+name +"!";
+ }
