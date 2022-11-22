8f6e17cd3a072a2704b31941c777f382c079ef5f
Daphne Cheng <dcheng11@ucsc.edu>
2022-11-21 21:05:26 -0800

    working meme api!

--- lab15/js/lab15.js
+++ lab15/js/lab15.js
@@ -4,60 +4,63 @@
  * Created:   11.21.2022
  *
  **/
-
  //URL = "https://api.imgflip.com/get_memes";
- //URL = "https://api.chucknorris.io/jokes/random";
- URL = "http://www.yerkee.com/api/fortune/wisdom";
- //GET "https://yesno.wtf/#api";
- //$("#output").append("<img src=" + data.image + " >");
+ var outputEl = document.getElementById("output");
 
+//event listner to when activate button is clicked
  $('#activate').click(function(){
    console.log("action button is clicked");
+   //starts ajax
    $.ajax({
-    url: URL,
-    //GET "/api/fortune/fortune",
-    //data: {fortune: ""},
-    type: "GET",
-    //dataType : "json",
-    //message: "GET Success",
-    success: function(data) {
-      console.log(data);
-    },
-    error: function () {
-      console.log("Error:");
-    }
+     //url of api we are getting from
+     url: "https://api.imgflip.com/get_memes",
+
+     type: "GET",
+     dataType : "json",
+      success: function(data) {
+        console.log(data);
+      },
+      error: function () {
+        console.log("Error:");
+      }
   })
+  //when ajax called data successfully
   .done(function(data) {
-    alert("Success!");
      //random number from 1-100
-     //var rand = 1 + Math.floor(Math.random() * 100);
-     console.log(data);
-
-
-     //console.log(rand);
-     //console.log("data.mm "+ data.memes);
-
-     //$("#title").html(data.title)
-     //$("#output").append("<h2>" + data.name + "<h2>");
-     //$("#output").append("<img src=" + data.icon_url + " >");
-     var printableData = "<pre>" + JSON.stringify(data) + "</pre>";
-     console.log(data.fortune);
-     $("#output").append("<p>" + data.fortune + "<p>");
-
+     var rand = 1 + Math.floor(Math.random() * 100);
+     //checking my printing stuff
+     console.log(rand);
+     console.log("name: " + data.data.memes[rand]["name"]);
+     //append meme to output div
+     $("#output").append("<h2>" + data.data.memes[rand]["name"] + "<h2>");
+     $("#output").append("<img src=" + data.data.memes[rand]["url"] + " >");
  })
 });
 
-     //console.log("mm.name "+memes.name);
-     //memes is not defined
 
-     //console.log("data.mm[r] "+data.memes[rand]);
-     //cannot read properties
+     // console.log("data.mm "+ data.memes);
+     // /undefined
+     //
+     // console.log("mm.name "+memes.name);
+     //  //memes is not defined
+     //
+     // console.log("data.mm[r] "+data.memes[rand]);
+     //  //cannot read properties
+     //
+     //  console.log("mm[r] "+memes[rand]);
+     //  //meme is not defined
+     //
+     //  console.log("data.mm[r].id "+data.memes[rand].id);
+     //  //cannot read properties
+     //
+     //$("#output").append("<p>" + data.fortune + "<p>");
+     //$("#title").html(data.title)
+     //$("#output").append("<h2>" + data.name + "<h2>");
+     //$("#output").append("<img src=" + data.icon_url + " >");
+     //var printableData = "<pre>" + JSON.stringify(data) + "</pre>";
+     //console.log(data);
 
-     //console.log("mm[r] "+memes[rand]);
-     //meme is not defined
-
-     //console.log("data.mm[r].id "+data.memes[rand].id);
-     //cannot read properties
-
-     //console.log("mm[r].id "+memes[rand].id);
-     //meme is not defined
+     //URL = "https://api.chucknorris.io/jokes/random";
+     //URL = "http://www.yerkee.com/api/fortune/wisdom";
+     //GET "https://yesno.wtf/#api";
+     //$("#output").append("<img src=" + data.image + " >");
